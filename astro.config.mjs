458ca08a0fdd28e10563212c7a560da7d3eeb81d
yoofus <zerowatts@tuta.io>
2025-07-25 15:07:14 -0400

    fix: :tada: keep it static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 
 import tailwindcss from "@tailwindcss/vite";
-import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: cloudflare(),
 	vite: {
 		plugins: [tailwindcss()],
 	},
